fb4785425acd577ff7d92f067127f19604187f2f
Robyn Hood <dominic.vargas93@gmail.com>
2022-01-12 17:08:22 -0800

    Getting weather to display to webpage

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const API_KEY = config.API_KEY;
-const units = 'imperial';
-const zipCode = '98631';
-const countryCode = 'us';
+let units = 'imperial';
+let zipCode = '';
+let countryCode = '';
+
+const zipInput = document.querySelector('.zipCode');
+const countryInput = document.querySelector('.countryCode');
+const submit = document.querySelector('.submit');
+const tempText = document.querySelector('.curTempText');
 
 const getWeather = async () => {
     try {
         const res = await axios.get(`http://api.openweathermap.org/data/2.5/weather?zip=${zipCode},${countryCode}&appid=${API_KEY}&units=${units}`)
-        console.log(res.data);
+        tempText.innerText = res.data.main.temp;
     } catch (e) {
         return 'An error has occured';
     }
 
 }
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    zipCode = zipInput.value;
+    countryCode = countryInput.value;
+    getWeather();
+})
