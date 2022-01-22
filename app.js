2abc4afa82304ae3eb91756ae41682e92f40d8aa
Robyn Hood <dominic.vargas93@gmail.com>
2022-01-21 21:59:48 -0800

    Added more ui elements

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ const zipInput = document.querySelector('.zipCode');
 const countryInput = document.querySelector('.countryCode');
 const submit = document.querySelector('.submit');
 const tempText = document.querySelector('.curTempText');
+const weatherLocation = document.querySelector('.location')
 
 const getWeather = async () => {
     try {
         const res = await axios.get(`http://api.openweathermap.org/data/2.5/weather?zip=${zipCode},${countryCode}&appid=${API_KEY}&units=${units}`)
         tempText.innerText = res.data.main.temp;
+        weatherLocation.innerText = `Weather for ${res.data.name}`;
+
     } catch (e) {
         return 'An error has occured';
     }
